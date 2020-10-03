b309b01c714a615b5367395d4760178909d45286
Salex406 <60296062+Salex406@users.noreply.github.com>
2020-10-03 13:48:48 +0300

    Update CtrlFetch.vhdl

--- part4/CtrlFetch.vhdl
+++ part4/CtrlFetch.vhdl
@@ -2,34 +2,26 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Spartan 3
---   Speed grade 4: 164 MHz
---   Speed grade 5: 189 MHz
--- Virtex 6
---   Speed grade 3: 628 MHz
--- Zync
---   Speed grade 3: 642 MHz
-
 -- Responsible for managing the program counter.
 entity CtrlFetch is
 port ( 
 	i_clk:			in std_logic;
 	i_reset:			in std_logic;
 	-- control path
-	i_mode12K:		in std_logic_vector(1 downto 0);
-	i_modeAddZA:	in std_logic_vector(1 downto 0);
-	i_modePCZ:		in std_logic;
-	i_loadPC:		in std_logic;
-	i_loadIR:		in std_logic;
-	-- data path
-	i_K:				in unsigned(15 downto 0);
+	i_mode12K:		in std_logic_vector(1 downto 0); -- if 00 adder +1, 01 adder +2, 10 adder 1 + K <i_K>
+	i_modeAddZA:	in std_logic_vector(1 downto 0); -- if 0 PC load from Adder, 01 load from Z to PC (for IJMP), 10 load from A(for ret) 
+	i_modePCZ:		in std_logic; -- if 1, will load Z reg to PM address (for command)
+	i_loadPC:		in std_logic; -- if 1, will load value to PC from MUX <modeAddZA>
+	i_loadIR:		in std_logic; -- if 1, will load value from 
+	-- data mem <i_PMDATA> to <s_IR> and at the next clk data will be at <o_IR>
+	i_K:				in unsigned(15 downto 0); -- input constant for RJMP K, PC WILL be k+1
 	i_A:				in unsigned(15 downto 0);
 	i_Z:				in unsigned(15 downto 0);
 	o_IR:				out unsigned(15 downto 0);
-	o_PMDATA:		out unsigned(15 downto 0);
+	o_PMDATA:		out unsigned(15 downto 0); --out of program memory (direct from <i_pmdata>>
 	-- memory interface
-	o_PMADDR:		out unsigned(15 downto 0);
-	i_PMDATA:		in unsigned(15 downto 0)
+	o_PMADDR:		out unsigned(15 downto 0); -- connected to addr input of memory
+	i_PMDATA:		in unsigned(15 downto 0) --connected to the output of program mem
 );
 end CtrlFetch;
 
